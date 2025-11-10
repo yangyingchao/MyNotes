c2d3bd3bf720e32fe49dda8cdc2a3cc9949916fb
yangyingchao <yang.yingchao@qq.com>
2025-11-10 17:27:17 +0800

    clean up date

---
+++ org/2022/05/.dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil . ((buffer-read-only . nil)
+         (fill-column . 112))))
